0fc0a5efbc1b10c9944cd3c743d5990e8d58e452
Pavel4684 <pavelterral@gmail.com>
2026-06-30 01:37:07 +0200

    Add main.jsx to render MontaureoPlatform component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import MontaureoPlatform from "./MontaureoPlatform.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MontaureoPlatform />
+  </React.StrictMode>
+);
